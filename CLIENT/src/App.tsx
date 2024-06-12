5f1a8da3a2c70633b115a5914b7cff249d2fd6fa
AAYUS KARKI <alexkarki2060@gmail.com>
2024-06-12 14:40:35 +0545

    utils setup for frequently used things

--- CLIENT/src/App.tsx
+++ CLIENT/src/App.tsx
@@ -1,30 +1,15 @@
-<<<<<<< HEAD
-import { useState } from 'react'
-
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import LandingPage from './pages/LandingPage'
 import Login from './pages/Login'
-
 function App() {
- 
-
-  return (
-    <>
-    {/* <LandingPage/> */}
-    <Login/>
-=======
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-
-function App() {
-
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" />
+          <Route path="/" element={<LandingPage />}/>
+          <Route path="/login" element={<Login />}/>
         </Routes>
       </BrowserRouter>
->>>>>>> 98d759cc664ebd17653c8ff6fb18bc765f326e65
     </>
   )
 }
--- CLIENT/src/main.tsx
+++ CLIENT/src/main.tsx
@@ -3,15 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import {Toaster} from 'react-hot-toast'
 import './index.css'
-import MentalHealthMCQ from './components/MCQ/mcq.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-<<<<<<< HEAD
-    <MentalHealthMCQ/>
-=======
     <Toaster/>
->>>>>>> f251945c1c13ddd040496fc7e19ef4988a082c63
   </React.StrictMode>,
 )
