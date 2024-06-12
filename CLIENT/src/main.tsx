a84c8109d9ff27189922fb7ac694ac5339fd8822
Aayush Niroula <ayushniroula644@gmail.com>
2024-06-12 14:49:13 +0545

    usermodel auth function added

--- CLIENT/src/main.tsx
+++ CLIENT/src/main.tsx
@@ -8,10 +8,7 @@ import MentalHealthMCQ from './components/MCQ/mcq.tsx'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-<<<<<<< HEAD
-    <MentalHealthMCQ/>
-=======
     <Toaster/>
->>>>>>> f251945c1c13ddd040496fc7e19ef4988a082c63
+    <MentalHealthMCQ/>
   </React.StrictMode>,
 )
