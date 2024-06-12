a3f4994e3284e0cfedc0ea39c83069c5447c2018
Sandesh laksamba <sandeshlaksamba123@gmail.com>
2024-06-12 16:51:16 +0545

    home

--- CLIENT/src/App.tsx
+++ CLIENT/src/App.tsx
@@ -1,12 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import LandingPage from './pages/LandingPage'
 import Login from './pages/Login'
-<<<<<<< HEAD
 import MentalHealthMCQ from "./components/MCQ/mcq"
 import HomePage from "./pages/HomePage"
-=======
 import SignUp from "./pages/SignUp"
->>>>>>> cbdef064e62e791e4346f29db866705f6cc02041
 function App() {
   return (
     <>
@@ -14,12 +11,9 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage />}/>
           <Route path="/login" element={<Login />}/>
-<<<<<<< HEAD
           <Route path="/mcq" element={<MentalHealthMCQ/>}/>
           <Route path="/HomePage" element={<HomePage/>}/>
-=======
           <Route path="/signup" element={<SignUp/>}/>
->>>>>>> cbdef064e62e791e4346f29db866705f6cc02041
         </Routes>
       </BrowserRouter>
     </>
