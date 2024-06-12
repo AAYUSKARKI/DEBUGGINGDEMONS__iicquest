78e26da2feb17a9f92473498d3147f07c1bd5e53
Aayush Niroula <ayushniroula644@gmail.com>
2024-06-12 16:52:36 +0545

    error

--- CLIENT/src/App.tsx
+++ CLIENT/src/App.tsx
@@ -1,12 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import LandingPage from './pages/LandingPage'
 import Login from './pages/Login'
-<<<<<<< HEAD
-import MentalHealthMCQ from "./components/MCQ/mcq"
-import HomePage from "./pages/HomePage"
-=======
 import SignUp from "./pages/SignUp"
->>>>>>> cbdef064e62e791e4346f29db866705f6cc02041
+
 function App() {
   return (
     <>
@@ -14,13 +10,11 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage />}/>
           <Route path="/login" element={<Login />}/>
-<<<<<<< HEAD
-          <Route path="/mcq" element={<MentalHealthMCQ/>}/>
-          <Route path="/HomePage" element={<HomePage/>}/>
-=======
           <Route path="/signup" element={<SignUp/>}/>
->>>>>>> cbdef064e62e791e4346f29db866705f6cc02041
+          
         </Routes>
+        
+       
       </BrowserRouter>
     </>
   )
