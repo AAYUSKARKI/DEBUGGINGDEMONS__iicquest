cbdef064e62e791e4346f29db866705f6cc02041
Sandesh laksamba <sandeshlaksamba123@gmail.com>
2024-06-12 15:11:01 +0545

    register

--- CLIENT/src/App.tsx
+++ CLIENT/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import LandingPage from './pages/LandingPage'
 import Login from './pages/Login'
+import SignUp from "./pages/SignUp"
 function App() {
   return (
     <>
@@ -8,6 +9,7 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage />}/>
           <Route path="/login" element={<Login />}/>
+          <Route path="/signup" element={<SignUp/>}/>
         </Routes>
       </BrowserRouter>
     </>
--- CLIENT/src/pages/Login.tsx
+++ CLIENT/src/pages/Login.tsx
@@ -6,21 +6,28 @@ function Login() {
     <section  className='flex justify-center items-center bg-black h-[100vh]'>
         <div>
            
-            <form className='flex flex-col bg-slate-300 gap-3 p-8 w-[400px] rounded-md' > 
+            <form className='flex flex-col bg-slate-300 gap-3 p-8 w-[400px] rounded-md shadow-md shadow-white' > 
                 <h1 className='m-auto font-bold text-blue-600'>LOGIN HERE</h1>
                 <div className='flex flex-col gap-1'>
                     <label >Email:</label>
                     <input type="text"
                       placeholder='enter your email '
-                      className='bg-slate-500 p-2 h-14  rounded-md' />
+                      className=' p-2 h-14  rounded-md' />
                 </div>
                 <div className='flex flex-col gap-1'>
                     <label >Password:</label>
-                    <input type="text"
+                    <input type="password"
                       placeholder='enter your password '
-                      className='bg-slate-500 p-2  h-14  rounded-md' />
+                      className=' p-2  h-14  rounded-md' />
                 </div>
-                <button className='bg-blue-700 p-2  h-14 rounded-md hover:bg-blue-600'>Login</button>
+                <br />
+                <button className='bg-blue-700 p-2  h-14 rounded-md hover:bg-blue-600 text-white font-bold'>Login</button>
+
+                <p className='m-auto text-blue-600'>Forgot password ?</p>
+<br />
+                
+
+                <button className='m-auto bg-green-500 p-4 rounded-xl shadow-xl shadow-slate-500'>Create New Account</button>
             </form>
         </div>
     </section>
