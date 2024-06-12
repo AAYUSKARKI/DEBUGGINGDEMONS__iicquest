91feffacf6eeee5accb8b4297dfd5523929675f9
Sandesh laksamba <sandeshlaksamba123@gmail.com>
2024-06-12 14:37:43 +0545

    change

--- CLIENT/src/App.tsx
+++ CLIENT/src/App.tsx
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-import { useState } from 'react'
-
-import './App.css'
-import LandingPage from './pages/LandingPage'
-import Login from './pages/Login'
-
-function App() {
- 
-
-  return (
-    <>
-    {/* <LandingPage/> */}
-    <Login/>
-=======
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
@@ -24,7 +9,6 @@ function App() {
           <Route path="/" />
         </Routes>
       </BrowserRouter>
->>>>>>> 98d759cc664ebd17653c8ff6fb18bc765f326e65
     </>
   )
 }
